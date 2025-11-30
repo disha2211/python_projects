a778f59d222600c5204227fb5ec264ddccd5bed3
Disha Kaushal <dishakaushal22@gmail.com>
2025-11-30 12:37:30 +0530

    simple program for pair of dice

--- dice_rolling_game/code.py
+++ dice_rolling_game/code.py
@@ -1,1 +1,10 @@
+import random
 
+while True:
+    x=random.randint(1,6)
+    y=random.randint(1,6)
+    ans=input(print("Roll the dice?(y/n):"))
+    if (ans=="y"):
+        print(f"({x},{y})")
+    else:
+        break
